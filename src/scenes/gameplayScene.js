44ef4f2eaee9e2164e740cd9e7d08132ee381ed2
Phildo <Phildo211@gmail.com>
2016-02-09 11:02:16 -0600

    wonky paralax

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -9,6 +9,9 @@ var GamePlayScene = function(game, stage)
 
   //utils
   var balloon_canv;
+  var cloud_canv;
+  var tree_canv;
+  var mountain_canv;
 
   //doqueues
   var dragger;
@@ -20,6 +23,12 @@ var GamePlayScene = function(game, stage)
   var balloon;
   var arrow;
   var pipes;
+
+  //scenery
+  var bg;
+  var mg;
+  var fg;
+  var ground;
 
   //ui
   var boost_pad;
@@ -40,17 +49,53 @@ var GamePlayScene = function(game, stage)
     balloon_canv.context.arc(balloon_canv.width/2,balloon_canv.height/2,balloon_canv.width/2,0,2*Math.PI);
     balloon_canv.context.fill();
 
+    cloud_canv = document.createElement('canvas');
+    cloud_canv.width = 100;
+    cloud_canv.height = 100;
+    cloud_canv.context = cloud_canv.getContext('2d');
+    cloud_canv.context.fillStyle = "#FFFFFF";
+    cloud_canv.context.beginPath();
+    cloud_canv.context.arc(cloud_canv.width/2,cloud_canv.height/2,cloud_canv.width/2,0,2*Math.PI);
+    cloud_canv.context.fill();
+
+    mountain_canv = document.createElement('canvas');
+    mountain_canv.width = 100;
+    mountain_canv.height = 100;
+    mountain_canv.context = mountain_canv.getContext('2d');
+    mountain_canv.context.fillStyle = "#888888";
+    mountain_canv.context.beginPath();
+    mountain_canv.context.moveTo(0,100);
+    mountain_canv.context.lineTo(50,0);
+    mountain_canv.context.lineTo(100,100);
+    mountain_canv.context.fill();
+
+    tree_canv = document.createElement('canvas');
+    tree_canv.width = 100;
+    tree_canv.height = 100;
+    tree_canv.context = tree_canv.getContext('2d');
+    tree_canv.context.fillStyle = "#996655";
+    tree_canv.context.fillRect(40,20,20,80);
+    tree_canv.context.fillStyle = "#00FF00";
+    tree_canv.context.beginPath();
+    tree_canv.context.arc(tree_canv.width/2,tree_canv.height/3,tree_canv.width/3,0,2*Math.PI);
+    tree_canv.context.fill();
+
     //doqueues
     dragger = new Dragger({source:stage.dispCanv.canvas});
     presser = new Presser({source:stage.dispCanv.canvas});
 
     camera = new Camera();
-    grid = new Grid();
-    balloon = new Balloon();
-    arrow = new Arrow();
+    grid = new Obj(0,0,10,10);
+    balloon = new Obj();
+    arrow = new Obj();
     pipes = [];
     for(var i = 0; i < n_pipes; i++)
-      pipes.push(new Pipe(i*5,Math.random()*2-1,1,10));
+      pipes.push(new Obj(i*5,Math.random()*2-1,1,10));
+
+    bg = []; for(var i = 0; i < 2; i++) { bg.push(new Obj(i*100,  8,  1,  1)); bg[i].draw = drawCloud;    }
+    mg = []; for(var i = 0; i < 2; i++) { mg.push(new Obj(i* 50,  5,  5, 10)); mg[i].draw = drawMountain; }
+    fg = []; for(var i = 0; i < 2; i++) { fg.push(new Obj(i* 10, -1,  5,  5)); fg[i].draw = drawTree;     }
+    ground = new Obj();
 
     boost_pad = new function()
     {
@@ -74,7 +119,7 @@ var GamePlayScene = function(game, stage)
     presser.flush();
 
     //temp
-    if(boost_pad.pressed) balloon.t += 0.001;
+    if(boost_pad.pressed) balloon.t += 0.01;
     else balloon.t = lerp(balloon.t,1,0.001);
 
     //accel     =         lift             weight
@@ -95,6 +140,15 @@ var GamePlayScene = function(game, stage)
     arrow.ww = 0;
     arrow.wh = balloon.wya*1000;
 
+    ground.wx = 0;
+    ground.wy = -1;
+    ground.ww = 1;
+    ground.wh = 2;
+
+    //collision
+    for(var i = 0; i < n_pipes; i++)
+      pipes[i].colliding = queryRectCollide(balloon,pipes[i]);
+
     //cam track
     camera.wx = balloon.wx;
     if(balloon.wy > 4)
@@ -107,44 +161,52 @@ var GamePlayScene = function(game, stage)
       camera.wh = 10+((-balloon.wy-4)*2);
       camera.ww = camera.wh;
     }
+
+    //faux parallax
+    for(var i = 0; i < bg.length; i++) bg[i].wx = bg[i].hwx+camera.wx*0.8;
+    for(var i = 0; i < mg.length; i++) mg[i].wx = mg[i].hwx+camera.wx*0.5;
+    for(var i = 0; i < fg.length; i++) fg[i].wx = fg[i].hwx+camera.wx*0.2;
+
+    //screen space resolution
     screenSpace(camera,dc,balloon);
     screenSpace(camera,dc,arrow);
-    for(var i = 0; i < n_pipes; i++)
-      screenSpace(camera,dc,pipes[i]);
+    for(var i = 0; i < n_pipes;   i++) screenSpace(camera,dc,pipes[i]);
+    for(var i = 0; i < bg.length; i++) screenSpace(camera,dc,bg[i]);
+    for(var i = 0; i < mg.length; i++) screenSpace(camera,dc,mg[i]);
+    for(var i = 0; i < fg.length; i++) screenSpace(camera,dc,fg[i]);
+    screenSpace(camera,dc,ground);
     screenSpace(camera,dc,grid);
-
-    //collision
-    for(var i = 0; i < n_pipes; i++)
-      pipes[i].colliding = queryRectCollide(balloon,pipes[i]);
   }
 
   self.draw = function()
   {
     //sky
-    dc.context.fillStyle = "#8899AA";
+    dc.context.fillStyle = "#8899BB";
     dc.context.fillRect(0,0,dc.width,dc.height);
+    for(var i = 0; i < bg.length; i++) bg[i].draw(bg[i]);
+    for(var i = 0; i < mg.length; i++) mg[i].draw(mg[i]);
+    //ground
+    dc.context.fillStyle = "#88FFAA";
+    dc.context.fillRect(0,ground.y,dc.width,dc.height-ground.y);
+    for(var i = 0; i < fg.length; i++) fg[i].draw(fg[i]);
 
-    //balloon
-    dc.context.drawImage(balloon_canv,balloon.x,balloon.y,balloon.w,balloon.h);
-    dc.context.fillStyle = "#000000";
-    dc.context.fillText(balloon.t,balloon.x,balloon.y+balloon.h);
+    drawBalloon(balloon);
+    drawArrow(arrow);
+    for(var i = 0; i < n_pipes; i++) drawPipe(pipes[i]);
+    drawGrid(grid);
 
-    //arrow
-    dc.context.beginPath();
-    dc.context.moveTo(arrow.x        -arrow.w/2,arrow.y        -arrow.h/2);
-    dc.context.lineTo(arrow.x+arrow.w-arrow.w/2,arrow.y+arrow.h-arrow.h/2);
-    dc.context.stroke();
-
-    //pipes
-    for(var i = 0; i < n_pipes; i++)
-    {
-      if(pipes[i].colliding) dc.context.fillStyle = "#FF5500";
-      else                   dc.context.fillStyle = "#005500";
-      dc.context.fillRect(pipes[i].x,pipes[i].y,pipes[i].w,pipes[i].h);
-    }
-
-    //ui
-    grid.draw();
+/*
+    var o = new Obj();
+    o.x = 10;
+    o.y = 10;
+    o.w = 10;
+    o.h = 10;
+    o.x = 130.5890909090917;
+    o.y = 87.27272727272727;
+    o.w = 290.9090909090909;
+    o.h = 145.45454545454544;
+    drawMountain(o);
+*/
   };
 
   self.cleanup = function()
@@ -161,84 +223,80 @@ var GamePlayScene = function(game, stage)
     self.wh = 10; //1 = -0.5 -> 0.5
   }
 
-  var Grid = function()
+  var drawObj = function(obj){};
+  var Obj = function(wx,wy,ww,wh)
   {
     var self = this;
 
-    self.wx = 0;
-    self.wy = 0;
-    self.ww = 10;
-    self.wh = 10;
-
-    self.draw = function()
-    {
-      for(var i = 0; i < 11; i++)
-      {
-        var x = lerp(self.x,self.x+self.w,i/10);
-        dc.context.beginPath();
-        dc.context.moveTo(x,self.y);
-        dc.context.lineTo(x,self.y+self.h);
-        dc.context.stroke();
-        var y = lerp(self.y,self.y+self.h,i/10);
-        dc.context.beginPath();
-        dc.context.moveTo(self.x,y);
-        dc.context.lineTo(self.x+self.w,y);
-        dc.context.stroke();
-      }
-    }
-  }
-
-  var Balloon = function()
-  {
-    var self = this;
+    if(wx == undefined) wx = 0;
+    if(wy == undefined) wy = 0;
+    if(ww == undefined) ww = 1;
+    if(wh == undefined) wh = 1;
 
     self.x = 0;
     self.y = 0;
     self.w = 0;
     self.h = 0;
 
-    self.wx = 0;
-    self.wy = 0;
-    self.ww = 1;
-    self.wh = 1;
-
-    self.wya = 0;
-    self.wxv = 0.01;
-    self.wyv = 0;
-    self.m = 1;
-    self.t = 1;
-  }
-  var Arrow = function()
-  {
-    var self = this;
-
-    self.x = 0;
-    self.y = 0;
-    self.w = 0;
-    self.h = 0;
-
-    self.wx = 0;
-    self.wy = 0;
-    //w/h better read as offx/offy
-    self.ww = 1;
-    self.wh = 1;
-  }
-
-  var Pipe = function(wx, wy, ww, wh)
-  {
-    var self = this;
-
-    self.x = 0;
-    self.y = 0;
-    self.w = 0;
-    self.h = 0;
+    //'home' world position (in case of need of relative positioning)
+    self.hwx = wx;
+    self.hwy = wy;
+    self.hww = ww;
+    self.hwh = wh;
 
     self.wx = wx;
     self.wy = wy;
     self.ww = ww;
     self.wh = wh;
 
+    self.wxa = 0;
+    self.wya = 0;
+    self.wxv = 0.01;
+    self.wyv = 0;
+    self.m = 1;
+    self.t = 1;
+
     self.colliding = false;
+    self.draw = drawObj; //to be replaced- hoping interpreter smart enough to pack w/ function ptr
   }
+
+  var drawBalloon = function(obj)
+  {
+    dc.context.drawImage(balloon_canv,obj.x,obj.y,obj.w,obj.h);
+    dc.context.fillStyle = "#000000";
+    dc.context.fillText(obj.t,obj.x,obj.y+obj.h);
+  }
+  var drawArrow = function(obj)
+  {
+    dc.context.beginPath();
+    dc.context.moveTo(obj.x      -obj.w/2,obj.y      -obj.h/2);
+    dc.context.lineTo(obj.x+obj.w-obj.w/2,obj.y+obj.h-obj.h/2);
+    dc.context.stroke();
+  }
+  var drawPipe = function(obj)
+  {
+    if(obj.colliding) dc.context.fillStyle = "#FF5500";
+    else              dc.context.fillStyle = "#005500";
+    dc.context.fillRect(obj.x,obj.y,obj.w,obj.h);
+  }
+  var drawGrid = function(obj)
+  {
+    for(var i = 0; i < 11; i++)
+    {
+      var x = lerp(obj.x,obj.x+obj.w,i/10);
+      dc.context.beginPath();
+      dc.context.moveTo(x,obj.y);
+      dc.context.lineTo(x,obj.y+obj.h);
+      dc.context.stroke();
+      var y = lerp(obj.y,obj.y+obj.h,i/10);
+      dc.context.beginPath();
+      dc.context.moveTo(obj.x,y);
+      dc.context.lineTo(obj.x+obj.w,y);
+      dc.context.stroke();
+    }
+  }
+  var drawCloud    = function(obj) { dc.context.drawImage(cloud_canv,obj.x,obj.y,obj.w,obj.h); }
+  var drawMountain = function(obj) { dc.context.drawImage(mountain_canv,obj.x,obj.y,obj.w,obj.h); }
+  var drawTree     = function(obj) { dc.context.drawImage(tree_canv,obj.x,obj.y,obj.w,obj.h); }
 };
 
