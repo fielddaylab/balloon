a4365aeb0f10c374c24bab9f9fb21edf231accde
Phildo <Phildo211@gmail.com>
2016-04-19 16:22:30 -0500

    specified 'start from' in section labels

--- src/scenes/chooseScene.js
+++ src/scenes/chooseScene.js
@@ -43,10 +43,10 @@ var ChooseScene = function(game, stage)
   self.draw = function()
   {
     dc.context.textAlign = "left";
-    btn_intro.draw(dc);     dc.context.fillStyle = "#000000"; dc.context.fillText("Intro",btn_intro.x+8,btn_intro.y+btn_intro.h-4);
-    btn_particles.draw(dc); dc.context.fillStyle = "#000000"; dc.context.fillText("Visualize Particles",btn_particles.x+8,btn_particles.y+btn_particles.h-4);
-    btn_forces.draw(dc);    dc.context.fillStyle = "#000000"; dc.context.fillText("Conflicting Forces",btn_forces.x+8,btn_forces.y+btn_forces.h-4);
-    btn_density.draw(dc);   dc.context.fillStyle = "#000000"; dc.context.fillText("Density",btn_density.x+8,btn_density.y+btn_density.h-4);
+    btn_intro.draw(dc);     dc.context.fillStyle = "#000000"; dc.context.fillText("Start from Intro",btn_intro.x+8,btn_intro.y+btn_intro.h-4);
+    btn_particles.draw(dc); dc.context.fillStyle = "#000000"; dc.context.fillText("Start from Visualize Particles",btn_particles.x+8,btn_particles.y+btn_particles.h-4);
+    btn_forces.draw(dc);    dc.context.fillStyle = "#000000"; dc.context.fillText("Start from Conflicting Forces",btn_forces.x+8,btn_forces.y+btn_forces.h-4);
+    btn_density.draw(dc);   dc.context.fillStyle = "#000000"; dc.context.fillText("Start from Density",btn_density.x+8,btn_density.y+btn_density.h-4);
     btn_free.draw(dc);      dc.context.fillStyle = "#000000"; dc.context.fillText("Free Play",btn_free.x+8,btn_free.y+btn_free.h-4);
     btn_standard.draw(dc);  dc.context.fillStyle = "#000000"; dc.context.fillText("Standard Play (Best: "+fdisp(game.standard_best,1)+"m)",btn_standard.x+8,btn_standard.y+btn_standard.h-4);
     btn_refuel.draw(dc);    dc.context.fillStyle = "#000000"; dc.context.fillText("Refuel Play (Best: "+fdisp(game.refuel_best,1)+"m)",btn_refuel.x+8,btn_refuel.y+btn_refuel.h-4);
